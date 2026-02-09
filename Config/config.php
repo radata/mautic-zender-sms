d76f922b6783e8eacd0b99f9645c1fa11dfdf533
Robert Golebiewski <rag.web@outlook.com>
2026-02-09 12:46:41 +0100

    Refactor integration service configuration for improved readability

--- Config/config.php
+++ Config/config.php
@@ -9,7 +9,25 @@ return [
     'services' => [
         'integrations' => [
             'mautic.integration.zendersms' => [
-                'class' => \MauticPlugin\ZenderSmsBundle\Integration\ZenderSmsIntegration::class,
+                'class'     => \MauticPlugin\ZenderSmsBundle\Integration\ZenderSmsIntegration::class,
+                'arguments' => [
+                    'event_dispatcher',
+                    'mautic.helper.cache_storage',
+                    'doctrine.orm.entity_manager',
+                    'request_stack',
+                    'router',
+                    'translator',
+                    'monolog.logger.mautic',
+                    'mautic.helper.encryption',
+                    'mautic.lead.model.lead',
+                    'mautic.lead.model.company',
+                    'mautic.helper.paths',
+                    'mautic.core.model.notification',
+                    'mautic.lead.model.field',
+                    'mautic.plugin.model.integration_entity',
+                    'mautic.lead.model.dnc',
+                    'mautic.lead.field.fields_with_unique_identifier',
+                ],
             ],
         ],
         'others' => [
